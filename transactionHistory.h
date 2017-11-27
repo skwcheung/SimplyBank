73d9d2d0705cb4bbfaea8f927ec250238030deba
Steven Cheung <skwcheun@uwaterloo.ca>
2017-11-26 22:45:31 -0500

    Compiling while in class formatting. Refactored from last commit no new code.

--- transactionHistory.h
+++ transactionHistory.h
@@ -3,11 +3,29 @@
 
 using namespace std;
 
-struct Transaction{
-    float time; //Current time in seconds since Jan 1 1970 from ctime function
-    string type; //Type of transaction
+class transactionHistory{
+    public:
+        transactionHistory();
+
+        // Contains transaction objects
+        struct Transaction{
+            float time; //Current time in seconds since Jan 1 1970 from ctime function
+            string type; //Type of transaction
+        };
+
+        // Node struct for linked list
+        struct Node{
+            Node* next;
+            Node* previous;
+            Transaction transaction;
+        };
+        
+        Node* head;
+        Node* startPoint;
+
+        bool recordTransaction(const Transaction* transaction);
 };
 
-bool recordTransaction(const Transaction* transaction);
+
 
 #endif
